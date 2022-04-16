805c3d78808079142e71b97d0816c4130232aa70
Dr. Orion Lawlor on Pi4 <oslawlor@alaska.edu>
2022-04-16 15:28:04 -0800

    Start kend as well as backend, suitable for running at boot.

--- autonomy/backend/run.sh
+++ autonomy/backend/run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+# Suitable for init scripts or login scripts (~/.config/autostart on gnome)
 export DISPLAY=:0
-export BEACON=10.10.10.100
+#export BEACON=10.10.10.100
 DIR="$( dirname $0)"
-#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}")">/dev/null 2>&1)"
-"${DIR}"/backend "$@"
+cd "${DIR}"
+(cd ../kend; date >> log; ./kend >> log) &
+(date >> log; ./backend "$@" >> log)
+
