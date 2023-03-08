31696450b3b82d868f5f33d2669bee0c562c2021
Blue Santa <bluesanta.northpole@gmail.com>
2023-03-07 15:06:12 -0900

    add commented code from old json project to lunatic_capture_data.cpp

--- autonomy/lunabug/lunatic_capture_data.cpp
+++ autonomy/lunabug/lunatic_capture_data.cpp
@@ -1,7 +1,26 @@
 /* Debug print the backend's encoder output */
 #include "aurora/lunatic.h"
 #include "nlohmann/json.hpp"
-#include <iostream>
+
+// Temporary libraries
+
+// #include <string>
+// #include <fstream>
+// #include <sstream>
+// #include <iomanip>
+// #include <iostream>
+// #include <filesystem>
+// #include <stdlib.h>
+// 
+// using json = nlohmann::json;
+// using std::cin;
+// using std::cout;
+// using std::endl;
+// using std::vector;
+// using std::string;
+// using std::ofstream;
+// using std::ifstream;
+// using std::istringstream;
 
 int main() {
     MAKE_exchange_drive_encoders();
@@ -16,6 +35,33 @@ int main() {
         change.print();
 
         // Create a new file in the tmp dir
+        // Old code to be reused
+        // Process the provided file by writing its data into an output file
+        // void processOutputFile(const string& filename, const vector< pair< string, int>>& data) {
+        // 
+        //     ofstream fout(filename);
+        // 
+        //     if (!fout) {
+        //         cout << "Error opening output file" << endl;
+        //         exit(0);
+        //     }
+        // 
+        //     // Ensure the longest length of any word is represented in the column format for each word
+        //     int len = (int)(data.size());
+        // 
+        //     int longestLen = 0;
+        // 
+        //     for (int i = 0; i < len; i++) {
+        //         if (longestLen < (int)data.at(i).first.size()) {
+        //             longestLen = (int)data.at(i).first.size();
+        //         }
+        //     }
+        // 
+        //     for (int i = 0; i < len; i++) {
+        //         fout << left << setw(longestLen) << data.at(i).first << " " << setw(5) << right << data.at(i).second << endl;
+        //     }
+        // 
+        // }
         
         last=cur;
         
