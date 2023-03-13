af1856bd3e5d0c43d97daf2c8817ac19f9a611f1
Blue Santa <bluesanta.northpole@gmail.com>
2023-03-13 00:21:06 -0800

    add createNewFile() to lunatic_capture_data.cpp

--- autonomy/lunabug/lunatic_capture_data.cpp
+++ autonomy/lunabug/lunatic_capture_data.cpp
@@ -20,6 +20,21 @@ using std::ofstream;
 
 using json = nlohmann::json;
 
+// Create a new output file for json data streaming
+ofstream createNewFile();
+
+// Capture the current time and return in string format
+string capture_time();
+
+// Capture the current date and return in string format
+string capture_date();
+
+// Find and replace within string (function copied from stackoverflow: https://tinyurl.com/48fvpu6n via Czarek Tomcza)
+std::string ReplaceString(std::string subject, const std::string& search, const std::string& replace);
+
+// Set data storage location
+const string& data_storage_location = "/tmp/data_exchange/data_capture/";
+
 int main() {
 
     // Initialize data capture for the drive encoders
@@ -27,35 +42,11 @@ int main() {
     aurora::drive_encoders last;
     last.left   =   last.right  =   0.0f;
 
-    // Create directory for the captured data
-    const string& data_storage_location     =   "/tmp/data_exchange/data_capture/";
-    std::filesystem::create_directory(data_storage_location);
+    // Create ofstream stream for json data streaming
+    ofstream fout = createNewFile();
 
-    // Capture current time and convert to string format
-    // Create variables
-    const auto now          = std::chrono::high_resolution_clock::now();
-    const auto now_         = std::chrono::system_clock::to_time_t(now);
-    const auto ms           = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
-    string ms_formatted     = std::to_string(ms.count());
-    stringstream curr_time_file;
-    std::string curr_filename = "lunatic_data_" + curr_time_file.str() + ms_formatted;
-    // Format variables into current time string
-    curr_time_file << std::put_time(std::localtime(&now_), "%Y_%m_%d_%H_%M_%S_");
-
-    // Test that curr_filename is accurate
-    cout << curr_filename << endl;
-
-    // Create path to new file
-    const string& curr_path = data_storage_location + curr_filename + ".json";
-    cout << curr_path << endl;
-
-    // Create a new file in the tmp dir
-    ofstream fout(curr_path);
-    
-    if (!fout) {
-        cout << "Error opening output file" << endl;
-        exit(-1);
-    }
+    // Initiate json structure in output stream
+    fout << "{}";
 
     while (true) {
 
@@ -75,19 +66,6 @@ int main() {
         const auto now          = std::chrono::high_resolution_clock::now();
         const auto now_         = std::chrono::system_clock::to_time_t(now);
         const auto ms           = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
-        string ms_formatted     = std::to_string(ms.count());
-        stringstream curr_time_json;
-        stringstream curr_date_json;
-
-        // Format milliseconds to three digits
-        while (ms_formatted.length() < 3) {
-            ms_formatted = ms_formatted.insert(0, "0");
-        }
-
-        // Format variables into current time string
-        curr_time_file << std::put_time(std::localtime(&now_), "%Y_%m_%d_%H_%M_%S_");
-        curr_time_json << std::put_time(std::localtime(&now_), "%H:%M:%S:");
-        curr_date_json << std::put_time(std::localtime(&now_), "%Y/%m/%d");
 
         // Craft the json output
         json output_stream;
@@ -112,4 +90,81 @@ int main() {
     }
 }
 
+ofstream createNewFile() {
 
+    // Create directory for the captured data
+    std::filesystem::create_directory(data_storage_location);
+
+    // Capture time
+    string time_file = capture_time();
+
+    // Capture date
+    string date_file = capture_date();
+
+    // Convert vars to file format
+    time_file = ReplaceStringInPlace(time_file, ":", "_");
+    date_file = ReplaceStringInPlace(date_file, "\/", "_");
+
+    // Craft filename
+    stringstream filename;
+    filename << date_file + "_" + time_file + ".json";
+
+    // Create filename
+    std::string filename = "lunatic_data_" + filename.str();
+
+    // Test that curr_filename is accurate
+    cout << curr_filename << endl;
+
+    // Create path to new file
+    const string& curr_path = data_storage_location + curr_filename;
+    cout << curr_path << endl;
+
+    // Create a new file in the tmp dir
+    ofstream fout(curr_path);
+    
+    if (!fout) {
+        cout << "Error opening output file" << endl;
+        exit(-1);
+    }
+
+    return fout;
+
+
+}
+
+string capture_time() {
+
+    // Capture current time
+    const auto file_timestamp          = std::chrono::high_resolution_clock::now();
+    const auto file_timestamp_         = std::chrono::system_clock::to_time_t(file_timestamp);
+    const auto file_ms           = std::chrono::duration_cast<std::chrono::milliseconds>(file_timestamp.time_since_epoch()) % 1000;
+
+    // Convert current time to string format
+    string file_ms_formatted     = std::to_string(file_ms.count());
+
+    string ms_formatted     = std::to_string(ms.count());
+    stringstream curr_time_json;
+    stringstream curr_date_json;
+
+    // Format milliseconds to three digits
+    while (ms_formatted.length() < 3) {
+        ms_formatted = ms_formatted.insert(0, "0");
+    }
+
+    // Format variables into current time string
+    curr_time_json << std::put_time(std::localtime(&now_), "%H:%M:%S:"); curr_date_json << std::put_time(std::localtime(&now_), "%Y/%m/%d");
+}
+
+string capture_date();
+
+// Find and replace within string (function copied from stackoverflow: https://tinyurl.com/48fvpu6n via Czarek Tomcza)
+std::string ReplaceString(std::string subject, const std::string& search, const std::string& replace) {
+
+        size_t pos = 0;
+            while ((pos = subject.find(search, pos)) != std::string::npos) {
+                         subject.replace(pos, search.length(), replace);
+                                  pos += replace.length();
+                                      }
+                return subject;
+
+}
