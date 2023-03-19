dadc7dbfa8bb57fc97e63b7c0baa33b39ca653e0
Blue Santa <bluesanta.northpole@gmail.com>
2023-03-19 06:27:33 -0800

    prep database insert command and capture time as uint from epoch instead

--- autonomy/lunaview/lunacapture.cpp
+++ autonomy/lunaview/lunacapture.cpp
@@ -47,7 +47,6 @@ int main() {
 
     if (!psql_conn.is_open()) {
         cout << "Connection to database could not be established." << endl;
-
         return 0;
     }
 
@@ -58,7 +57,7 @@ int main() {
 
             pqxx::work w(psql_conn);
 
-            // TO-DO
+            // TO DO
             // Establish variables for names of db, columns, etc.
             // (Table is to contain most data in a single json string
             // and, likely, also a timestamp column to indicate 
@@ -74,7 +73,9 @@ int main() {
             w.commit();
 
         } else {
+
             cout << db_disconnect_msg << endl;
+
         }
 
     } catch (const std::exception& e) {
@@ -82,6 +83,7 @@ int main() {
         cout << e.what() << endl;
         
         return 0;
+
     }
 
     // From robot_base.h (included in lunatic.h), obtain info from class robot_base
@@ -98,14 +100,11 @@ int main() {
 
     while (true) {
 
-        // May not need to print this to terminal
-        // // If there has been a change to the drive encoders, update terminal printout text for clarity
-        // if (exchange_drive_encoders.updated()) printf("+");
-
+        // Update the backend data
         exchange_drive_encoders.updated();
         state = exchange_backend_state.read();
 
-        // TO-DO Simplify code by putting all of the below variable/json building into a separate function
+        // TO DO Simplify code by putting all of the below variable/json building into a separate function
         // and returning the result as final string/json value
 
         // Calculate amount of change in drive encoders
@@ -114,40 +113,43 @@ int main() {
         aurora::drive_encoders change   =   cur - last;
 
         // Craft the json output
-        json output_stream;
-        output_stream["date_robot"]   = capture_date();
-        output_stream["time_robot"]   = capture_time();
-        output_stream["drive_encoder_left"]   = to_string(change.left);
-        output_stream["drive_encoder_right"]  = to_string(change.right);
+        json output_json;
+
+        // Capture epoch time
+        output_json["epoch_time"]   = capture_epoch();
+
+        // Capture drive encoder change data
+        output_json["drive_encoder_left"]   = change.left;
+        output_json["drive_encoder_right"]  = change.right;
 
         // List of joints, in sequential order, from base to furthest point of arm
         // Vars are all of type float
         // These are all angles reconstructed from the inertial measurement units (IMUs)
-        output_stream["fork"]  = state.joint.angle.fork;
-        output_stream["dump"]  = state.joint.angle.dump;
-        output_stream["boom"]  = state.joint.angle.boom;
-        output_stream["stick"] = state.joint.angle.stick;
-        output_stream["tilt"]  = state.joint.angle.tilt;
-        output_stream["spin"]  = state.joint.angle.spin;
+        output_json["fork"]  = state.joint.angle.fork;
+        output_json["dump"]  = state.joint.angle.dump;
+        output_json["boom"]  = state.joint.angle.boom;
+        output_json["stick"] = state.joint.angle.stick;
+        output_json["tilt"]  = state.joint.angle.tilt;
+        output_json["spin"]  = state.joint.angle.spin;
 
         // This is the power being sent to the motor, not necessarily position
         // Vars are all of type float
         // Values run from -1 to +1, and indicate full backward to full forward 
         // The first two indicate power to the drive motors
-        output_stream["power_left"]     = state.power.left;
-        output_stream["power_right"]    = state.power.right;
+        output_json["power_left"]     = state.power.left;
+        output_json["power_right"]    = state.power.right;
         // These variables indicate power to the joints
-        output_stream["power_fork"]     = state.power.fork;
-        output_stream["power_dump"]     = state.power.dump;
-        output_stream["power_boom"]     = state.power.boom;
-        output_stream["power_stick"]    = state.power.stick;
-        output_stream["power_tilt"]     = state.power.tilt;
-        output_stream["power_spin"]     = state.power.spin;
+        output_json["power_fork"]     = state.power.fork;
+        output_json["power_dump"]     = state.power.dump;
+        output_json["power_boom"]     = state.power.boom;
+        output_json["power_stick"]    = state.power.stick;
+        output_json["power_tilt"]     = state.power.tilt;
+        output_json["power_spin"]     = state.power.spin;
         // This var represents power level sent to tool
-        output_stream["power_tool"]     = state.power.tool;
+        output_json["power_tool"]     = state.power.tool;
 
         // The state.state variable is one int
-        output_stream["state_state"]     = state.state;
+        output_json["state_state"]     = state.state;
 
         // state.sensor is obsolete and therefore currently omitted
         // Later will include info such as battery voltage
@@ -155,62 +157,66 @@ int main() {
         // The state.loc variables represent an estimate of location 
         // Values are of type float
         // Variables (x, y) are in meters
-        output_stream["loc_x"]          = state.loc.x;
-        output_stream["loc_y"]          = state.loc.y;
-        // Variable (angle) are in degrees
-        output_stream["loc_angle"]      = state.loc.angle;
+        output_json["loc_x"]          = state.loc.x;
+        output_json["loc_y"]          = state.loc.y;
+        // Variable (angle) is in degrees
+        output_json["loc_angle"]      = state.loc.angle;
 
         // Test that json is formatted properly:
     
-        string test = output_stream.dump();
-        cout << test << endl;
+        cout << output_json.dump() << endl;
 
-        // TO-DO Prep insert command for database
+        // TO DO Prep insert command for database
 
         // Reset 
         last=cur;
-        aurora::data_exchange_sleep(100);
+        aurora::data_exchange_sleep(500);
     }
 }
 
-// Capture current time
-string capture_time() {
+// Capture current epoch time
+uint capture_epoch() {
 
     // Capture current time
     const auto timestamp           = std::chrono::high_resolution_clock::now();
-    const auto timestamp_          = std::chrono::system_clock::to_time_t(timestamp);
-    const auto ms                  = std::chrono::duration_cast<std::chrono::milliseconds>(timestamp.time_since_epoch()) % 1000;
+    // const auto timestamp_          = std::chrono::system_clock::to_time_t(timestamp);
+    // const auto ms                  = std::chrono::duration_cast<std::chrono::milliseconds>(timestamp.time_since_epoch()) % 1000;
+    const auto epoch_time          = std::chrono::duration_cast<std::chrono::milliseconds>(timestamp.time_since_epoch());
+
+
+    uint result = epoch_time.count();
 
     // Convert current time to string format
-    string ms_formatted            = std::to_string(ms.count());
-    stringstream time_output;
+    // string ms_formatted            = std::to_string(ms.count());
 
     // Format milliseconds to three digits
-    while (ms_formatted.length() < 3) {
-        ms_formatted = ms_formatted.insert(0, "0");
-    }
+    // while (ms_formatted.length() < 3) {
+    //     ms_formatted = ms_formatted.insert(0, "0");
+    // }
 
     // Format variables into current time string
-    time_output << std::put_time(std::localtime(&timestamp_), "%H:%M:%S:") << ms_formatted;
+    // time_output << std::put_time(std::localtime(&timestamp_), "%H:%M:%S:") << ms_formatted;
 
-    return time_output.str();
+    // return time_output.str();
+
+    return result;
 }
 
-// Capture current date
-string capture_date() {
-    // Capture current date
-    const auto datestamp           = std::chrono::high_resolution_clock::now();
-    const auto datestamp_          = std::chrono::system_clock::to_time_t(datestamp);
-
-    // Convert current time to string format
-    stringstream date_output;
-
-    // Format variables into current time string
-    date_output << std::put_time(std::localtime(&datestamp_), "%Y/%m/%d");
-
-    return date_output.str();
-
-}
+// // Capture current date
+// string capture_date() {
+//     // Capture current date
+//     const auto datestamp           = std::chrono::high_resolution_clock::now();
+//     const auto datestamp_          = std::chrono::system_clock::to_time_t(datestamp);
+// 
+//     // Convert current time to string format
+//     stringstream date_output;
+// 
+//     // Format variables into current time string
+//     date_output << std::put_time(std::localtime(&datestamp_), "%Y/%m/%d");
+// 
+//     return date_output.str();
+// 
+// }
 
 // Find and replace within string (function copied from stackoverflow: https://tinyurl.com/48fvpu6n via Czarek Tomcza)
 std::string ReplaceString(std::string subject, const std::string& search, const std::string& replace) {
@@ -234,7 +240,7 @@ void prepare_transactions(pqxx::connection &psql_conn) {
     // Prepare statement to insert data into table
     psql_conn.prepare(
             "insert_data",
-            "INSERT INTO $1 ( $2 ) VALUES ( $3 );"
+            "INSERT INTO $1 ( $2 ) VALUES ( '$3'::jsonb );"
     );
 
 };
