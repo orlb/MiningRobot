7efb9370991880ce56c9d5a1d83621d4e6ca0777
Orion Lawlor <lawlor@alaska.edu>
2023-03-21 13:04:44 -0800

    Move inverse kinematics into kinematic_links, including tool angle.

--- autonomy/kinematics/ik/ik_test.cpp
+++ autonomy/kinematics/ik/ik_test.cpp
@@ -1,93 +1,20 @@
 /* 
- Inverse kinematics testing
+ Inverse kinematics testing: generate random joint angles, 
+ compute forward kinematics to get tool position, then compute
+ inverse kinematics to get back to joint angles.
+ 
+ This is useful for developing and debugging the kinematics code.
+ 
+ Dr. Orion Lawlor, lawlor@alaska.edu, 2023-03 (Public Domain)
 */
 #include <stdio.h>
 #include "aurora/lunatic.h"
 #include "aurora/kinematics.h"
 #include "aurora/kinematic_links.cpp"
 
-
-namespace aurora {
-
-/**
- Solves inverse kinematics (positions to joint angles)
- problems for the excahauler robot.  This robot is mostly 2D motion
- in the YZ plane, so it's much easier than general IK. 
-*/
-class excahauler_IK {
-public:
-	/** Given a 3D vector in frame coords, return the angle of this
-	 direction vector in the YZ plane.
-	 The Y axis has an angle of 0, the Z axis has an angle of +90 degrees.
-	 Angle is returned in degrees above the Y axis.
-	*/
-	static float frame_degrees(const vec3 &v) {
-		float rad=atan2(v.z,v.y);
-		return RAD2DEG*rad;
-	}
-
-
-	/**
-	  Given a 3D vector for the origin of the tilt link at the end of the stick,
-	  update the boom and stick joint angles to reach that point,
-	  Returns 1 if the joint was reachable, -1 if too far
-	  (Future: -2 if collision?)
-	*/
-	int solve_tilt(robot_joint_state &joint, const vec3 &tilt_loc)
-	{
-		vec3 tilt_rel = tilt_loc - boomG.origin; // to target from boom start
-		float tilt_len = length(tilt_rel);
-		float tilt_deg = frame_degrees(tilt_rel);
-		
-		// Use law of cosines to solve for the angle from boom to tilt (BT)
-		//  Side a = boom
-		//  Side b = tilt
-		/// Side c = stick
-		float a=boom_len, b=tilt_len, c=stick_len;
-		float cos_tb = (a*a + b*b - c*c)/(2.0f*a*b);
-		if (cos_tb>1.0f || cos_tb<-1.0f) return -1; // no good
-		float tb_deg=RAD2DEG*acos(cos_tb);
-		joint.angle.boom=tilt_deg + tb_deg - boom_start; // frame to boom = frame to tilt - boom to tilt
-        
-        // Use law of cosines again on angle from stick to boom (SB)
-		float cos_sb = (a*a + c*c - b*b)/(2.0f*a*c);
-		if (cos_sb>1.0f || cos_sb<-1.0f) return -1; // no good
-		float sb_deg=RAD2DEG*acos(cos_sb);
-		joint.angle.stick=sb_deg - stick_start + boom_start - 180.0f ;
-        
-		return 1;
-	}
-
-
-	excahauler_IK() 
-		:frameG(link_geometry(link_frame)),
-		 boomG(link_geometry(link_boom)),
-		 stickG(link_geometry(link_stick)),
-		 tiltG(link_geometry(link_tilt))
-	{
-		boom_len=length(stickG.origin); // boom connects frame to stick
-		stick_len=length(tiltG.origin); // stick connects boom to tilt
-		boom_start=frame_degrees(stickG.origin);
-		stick_start=frame_degrees(tiltG.origin);
-	}
-
-
-private:
-	const robot_link_geometry &frameG, &boomG, &stickG, &tiltG; 
-	// Length of arm links relative to frame
-	float boom_len, stick_len;
-	// Angle of arm link origins
-	float boom_start, stick_start;
-
-};
-
-
-
-} // end namespace
-
-
 using namespace aurora;
 
+// Make a random float between lo and hi.  Calls rand()
 float rand_float(float lo,float hi)
 {
     int limit=0xfffff;
@@ -98,20 +25,27 @@ float rand_float(float lo,float hi)
 int main() {
     MAKE_exchange_backend_state();
     excahauler_IK ik;
+    long fail_count=0;
     
-    srand(5);
     for (int rep=0;rep<10000;rep++) 
     {
+        srand(rep); //<- allow us to jump back to this pseudorandom test
+        
         // Make a test point using known angles
         robot_joint_state test_joint={0};
         test_joint.angle.stick=rand_float(+1.0,+72.0);
         test_joint.angle.boom=rand_float(-58.0,+63.0);
+        test_joint.angle.tilt=rand_float(-77.2, +59.7);
         
-        robot_link_coords test_fk(test_joint);
-        vec3 tilt = test_fk.coord3D(link_tilt).origin;
+        // Forward kinematics
+        robot_link_coords test_fk(test_joint); 
+        const robot_coord3D &fk_tilt=test_fk.coord3D(link_tilt);
+        vec3 tilt = fk_tilt.origin;
+        float tilt_deg = excahauler_IK::frame_degrees(fk_tilt.Y);
         
+        // Test inverse solve back to tilt coords:
         robot_joint_state joint={0};
-        int ret = ik.solve_tilt(joint,tilt);
+        int ret = ik.solve_tilt(joint,tilt,tilt_deg);
         bool fail=true;
         if (ret>0) { // successful solve
             // Recompute forward kinematics (verifies angles are correct)
@@ -119,22 +53,36 @@ int main() {
             vec3 re = fk.coord3D(link_tilt).origin; // recompute tilt origin
             
             // Compute error on reconstructed position
-            float epsilon_m=1.0e-5; // error tolerance (meters)
-            float epsilon_deg=0.001; // error tolerance (degrees)
+            float epsilon_m=1.0e-4; // error tolerance (meters)
+            float epsilon_deg=0.01; // error tolerance (degrees)
             if (length(tilt - re)<epsilon_m &&
                 fabs(joint.angle.boom-test_joint.angle.boom)<epsilon_deg &&
-                fabs(joint.angle.stick-test_joint.angle.stick)<epsilon_deg
+                fabs(joint.angle.stick-test_joint.angle.stick)<epsilon_deg &&
+                fabs(joint.angle.tilt-test_joint.angle.tilt)<epsilon_deg
                ) 
             { // all values look good--test pass
                fail=false;
             }
             
-            if (fail || rep%1000 == 0) // print angles occasionally
-            printf("Tilt %.3f %.3f  -> Angle BS %.1f  %.1f -> %.3f %.3f\n", 
+            if (fail) // || rep%1000 == 0) // print angles occasionally
+            printf("Tilt %.3f %.3f  -> Angle BST %.1f  %.1f  %.1f -> %.3f %.3f\n", 
                    tilt.y, tilt.z,
-                   joint.angle.boom, joint.angle.stick,
+                   joint.angle.boom, joint.angle.stick, joint.angle.tilt,
                    re.y, re.z);
 
+        }
+        
+        // Test tool tip positioning:
+        robot_coord3D grinder = test_fk.coord3D(link_grinder);
+        //robot_coord3D fk_tilt = test_fk.coord3D(link_tilt);
+        robot_coord3D ik_tilt = robot_link_coords::parent_from_child(link_tilt, link_grinder, grinder);
+        vec3 grinder_in_tilt = fk_tilt.local_from_world(grinder.origin);
+        if (length(grinder_in_tilt - ik_tilt.origin)>0.0001) {
+            fail=true;
+            printf(" fk_tilt: ");
+            fk_tilt.print();
+            printf(" ik_tilt: ");
+            ik_tilt.print();
         }
         
         if (fail) {
@@ -142,8 +90,11 @@ int main() {
             printf(" IK fail %d: test BS %.1f  %.1f\n",
                 ret, 
                 test_joint.angle.boom, test_joint.angle.stick);
+            fail_count++;
         }
     }
-
+    
+    printf("Tests finished: %ld failures\n",fail_count);
+    return 0;
 }
 
