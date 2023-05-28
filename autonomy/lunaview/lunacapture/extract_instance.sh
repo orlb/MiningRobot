4c70fc71df6675ad8cc1c3313c248343d3512404
Blue Santa <golden@bluesanta.io>
2023-05-28 01:39:36 -0800

    added warnings and prompts to console

--- autonomy/lunaview/lunacapture/extract_instance.sh
+++ autonomy/lunaview/lunacapture/extract_instance.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # Create (or update) the postgres database used to store robot data.
 
+ if [ -z "$*" ] ; then
+     echo "Please provide the instance number that you wish to export as a commandline argument\nFor example: ./extract_instance.sh 1"
+     exit 0
+ fi
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 sudo mkdir $SCRIPT_DIR/file_exports
@@ -17,3 +22,7 @@ SQL
 EOF
 
 sudo mv /tmp/$1.csv $SCRIPT_DIR/file_exports/
+
+if test -f $SCRIPT_DIR/file_exports/$1.csv; then
+    echo "Exported instance number $1 to ./file_exports/$1.csv"
+fi
