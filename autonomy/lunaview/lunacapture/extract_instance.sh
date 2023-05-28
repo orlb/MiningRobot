bda294b3de30a15dccbbc36d754b05b4fe9a7699
Blue Santa <golden@bluesanta.io>
2023-05-28 01:30:18 -0800

    update to export as .csv

--- autonomy/lunaview/lunacapture/extract_instance.sh
+++ autonomy/lunaview/lunacapture/extract_instance.sh
@@ -3,17 +3,17 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-sudo mkdir file_exports
+sudo mkdir $SCRIPT_DIR/file_exports
 
 sudo -su postgres << EOF
 
 psql << SQL
 
 \c test_cpp
-\copy (SELECT * FROM test_conn WHERE instance_num = $1) TO '/tmp/$1.json' WITH DELIMITER ','
+\COPY (SELECT * FROM test_conn WHERE instance_num = $1) TO '/tmp/$1.csv' WITH DELIMITER ',' CSV HEADER
 
 SQL
 
 EOF
 
-sudo mv /tmp/$1.json $SCRIPT_DIR/file_exports/
+sudo mv /tmp/$1.csv $SCRIPT_DIR/file_exports/
